bdd459c4adccea4aa74ba36dd13a44b63230f948
ColtonBaker <cbaker7@memphis.edu>
2017-02-09 11:23:31 -0600

    First commit

---
+++ ctut.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int myFavNums [5];
+
+    int BadNums[5] =
+    {
+    4, 13, 14, 24, 34
+    };
+
+    cout << "Bad number 1: " << BadNums[4] <<endl;
+
+    char myName[6][6] =
+    {
+    {'C', 'o', 'l', 't', 'o', 'n'},
+    {'B', 'a', 'k', 'e', 'r', 'o' }
+    };
+
+    cout << "2nd Letter in 2nd array is " << myName[1][1] << endl;
+
+    myName[0][3] = 'l';
+
+    cout << myName[0][3];
+
+return 0;
+}
